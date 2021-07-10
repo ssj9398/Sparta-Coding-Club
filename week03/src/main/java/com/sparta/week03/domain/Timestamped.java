29efbc2aa1327252b9fef3a06c43cbf3a91df99c
alisyabob <sjs939883@gmail.com>
2021-07-10 21:21:38 +0900

    타임스탬프 getter & get 화면 추가

--- week03/src/main/java/com/sparta/week03/domain/Timestamped.java
+++ week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -1,5 +1,6 @@
 package com.sparta.week03.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
 @EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
+@Getter
 public abstract class Timestamped {
 
     @CreatedDate
