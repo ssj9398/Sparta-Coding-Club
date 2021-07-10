b976dc80a1528df25229a9718d78b4cf7b6f82e6
alisyabob <sjs939883@gmail.com>
2021-07-10 12:02:57 +0900

    컨트롤러 생성(기본crud)

---
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -0,0 +1,42 @@
+package com.sparta.week03.controller;
+
+import com.sparta.week03.domain.Memo;
+import com.sparta.week03.domain.MemoRepository;
+import com.sparta.week03.domain.MemoRequestDto;
+import com.sparta.week03.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+
+public class MemoController {
+    private final MemoRepository memoRepository;
+    private final MemoService memoService;
+
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto){
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
+    }
+
+    @GetMapping("/api/memos")
+    public List<Memo> readMemo(){
+        return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @PutMapping("/api/momos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        memoService.update(id, requestDto);
+        return id;
+    }
+
+    @DeleteMapping("/api/momos/{id}")
+    public Long deleteMemo(@PathVariable Long id){
+        memoRepository.deleteAll();
+        return id;
+    }
+}
