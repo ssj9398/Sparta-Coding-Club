85704682bbc3dde26de330f842765a42230b3a46
alisyabob <sjs939883@gmail.com>
2021-07-14 12:53:20 +0900

    검색 결과 문자열 DTO로 변환하기

--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,10 +1,12 @@
 package com.sparta.week04.utils;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 public class NaverShopSearch {
-    public String search() {
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Naver-Client-Id", "dNjjMimGLHEimMD7mo0i");
@@ -12,7 +14,7 @@ public class NaverShopSearch {
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=아디다스", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query="+query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -24,6 +26,18 @@ public class NaverShopSearch {
 
     public static void main(String[] args) {
         NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+        String result = naverShopSearch.search("아이폰");
+        JSONObject rjson = new JSONObject(result);
+        System.out.println(rjson);
+        JSONArray items = rjson.getJSONArray("items");
+        for(int i=0; i<items.length(); i++){
+            JSONObject itemJson = items.getJSONObject(i);
+            System.out.println(itemJson);
+            String title = itemJson.getString("title");
+            String image = itemJson.getString("image");
+            int lprice = itemJson.getInt("lprice");
+            String link = itemJson.getString("link");
+
+        }
     }
 }
