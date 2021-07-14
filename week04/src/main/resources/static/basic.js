960131ae92a7559ad47f9bdd8ea7e3342fa1cda7
alisyabob <sjs939883@gmail.com>
2021-07-15 00:29:26 +0900

    html, css, js 추가

---
+++ week04/src/main/resources/static/basic.js
@@ -0,0 +1,106 @@
+let targetId;
+
+$(document).ready(function () {
+    // id 가 query 인 녀석 위에서 엔터를 누르면 execSearch() 함수를 실행하라는 뜻입니다.
+    $('#query').on('keypress', function (e) {
+        if (e.key == 'Enter') {
+            execSearch();
+        }
+    });
+    $('#close').on('click', function () {
+        $('#container').removeClass('active');
+    })
+
+    $('.nav div.nav-see').on('click', function () {
+        $('div.nav-see').addClass('active');
+        $('div.nav-search').removeClass('active');
+
+        $('#see-area').show();
+        $('#search-area').hide();
+    })
+    $('.nav div.nav-search').on('click', function () {
+        $('div.nav-see').removeClass('active');
+        $('div.nav-search').addClass('active');
+
+        $('#see-area').hide();
+        $('#search-area').show();
+    })
+
+    $('#see-area').show();
+    $('#search-area').hide();
+
+    showProduct();
+})
+
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+///// 여기 아래에서부터 코드를 작성합니다. ////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+function execSearch() {
+    /**
+     * 검색어 input id: query
+     * 검색결과 목록: #search-result-box
+     * 검색결과 HTML 만드는 함수: addHTML
+     */
+    // 1. 검색창의 입력값을 가져온다.
+    // 2. 검색창 입력값을 검사하고, 입력하지 않았을 경우 focus.
+    // 3. GET /api/search?query=${query} 요청
+    // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기!
+
+}
+
+function addHTML(itemDto) {
+    /**
+     * class="search-itemDto" 인 녀석에서
+     * image, title, lprice, addProduct 활용하기
+     * 참고) onclick='addProduct(${JSON.stringify(itemDto)})'
+     */
+    return ``
+}
+
+function addProduct(itemDto) {
+    /**
+     * modal 뜨게 하는 법: $('#container').addClass('active');
+     * data를 ajax로 전달할 때는 두 가지가 매우 중요
+     * 1. contentType: "application/json",
+     * 2. data: JSON.stringify(itemDto),
+     */
+    // 1. POST /api/products 에 관심 상품 생성 요청
+    // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정 (숙제로 myprice 설정하기 위함)
+}
+
+function showProduct() {
+    /**
+     * 관심상품 목록: #product-container
+     * 검색결과 목록: #search-result-box
+     * 관심상품 HTML 만드는 함수: addProductItem
+     */
+    // 1. GET /api/products 요청
+    // 2. 관심상품 목록, 검색결과 목록 비우기
+    // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기!
+}
+
+function addProductItem(product) {
+    // link, image, title, lprice, myprice 변수 활용하기
+    return ``;
+}
+
+function setMyprice() {
+    /**
+     * 숙제! myprice 값 설정하기.
+     * 1. id가 myprice 인 input 태그에서 값을 가져온다.
+     * 2. 만약 값을 입력하지 않았으면 alert를 띄우고 중단한다.
+     * 3. PUT /api/product/${targetId} 에 data를 전달한다.
+     *    주의) contentType: "application/json",
+     *         data: JSON.stringify({myprice: myprice}),
+     *         빠뜨리지 말 것!
+     * 4. 모달을 종료한다. $('#container').removeClass('active');
+     * 5, 성공적으로 등록되었음을 알리는 alert를 띄운다.
+     * 6. 창을 새로고침한다. window.location.reload();
+     */
+}
