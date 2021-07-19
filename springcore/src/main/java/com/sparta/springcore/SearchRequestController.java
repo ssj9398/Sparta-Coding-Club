368b9efb96797f0b92d2cfc1038399e4a15bd160
alisyabob <sjs939883@gmail.com>
2021-07-19 17:01:25 +0900

    네이버 상품 검색 api 구현

---
+++ springcore/src/main/java/com/sparta/springcore/SearchRequestController.java
@@ -0,0 +1,25 @@
+package com.sparta.springcore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController // JSON으로 응답함을 선언합니다.
+public class SearchRequestController {
+
+    private final NaverShopSearch naverShopSearch;
+
+    @Autowired
+    public SearchRequestController(NaverShopSearch naverShopSearch) {
+        this.naverShopSearch = naverShopSearch;
+    }
+
+    @GetMapping("/api/search")
+    public List<ItemDto> getItems(@RequestParam String query) {
+        String resultString = naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(resultString);
+    }
+}
---
+++ springcore/src/test/java/com/sparta/springcore/SpringcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
