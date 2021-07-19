631c78a2eada2dd7560d97c8065e50f6985b9e17
alisyabob <sjs939883@gmail.com>
2021-07-19 18:47:42 +0900

    매번 생성하던 서비스를 가장위에 선언해 재사용

--- springcore/src/main/java/com/sparta/springcore/ProductController.java
+++ springcore/src/main/java/com/sparta/springcore/ProductController.java
@@ -9,10 +9,18 @@ import java.util.List;
 @RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
+    // 멤버 변수 선언
+    private final ProductService productService;
+
+    // 생성자: ProductController() 가 생성될 때 호출됨
+    public ProductController() {
+        // 멤버 변수 생성
+        productService = new ProductService();
+    }
+
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() throws SQLException {
-        ProductService productService = new ProductService();
         List<Product> products = productService.getProducts();
         // 응답 보내기
         return products;
@@ -21,7 +29,6 @@ public class ProductController {
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         Product product = productService.createProduct(requestDto);
         // 응답 보내기
         return product;
@@ -30,7 +37,6 @@ public class ProductController {
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         Product product = productService.updateProduct(id, requestDto);
         return product.getId();
     }
