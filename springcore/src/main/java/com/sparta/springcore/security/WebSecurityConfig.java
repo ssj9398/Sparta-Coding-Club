8e77327b08908d1007cb230cce66e31084a25540
alisyabob <sjs939883@gmail.com>
2021-07-20 01:51:55 +0900

    비밀번호 암호화 처리

--- springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package com.sparta.springcore.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
--- springcore/src/main/java/com/sparta/springcore/service/UserService.java
+++ springcore/src/main/java/com/sparta/springcore/service/UserService.java
@@ -12,23 +12,27 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
 
     public void registerUser(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
         // 회원 ID 중복 확인
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
         }
 
+        // 패스워드 인코딩
+        String password = passwordEncoder.encode(requestDto.getPassword());
         String email = requestDto.getEmail();
         // 사용자 ROLE 확인
         UserRole role = UserRole.USER;
