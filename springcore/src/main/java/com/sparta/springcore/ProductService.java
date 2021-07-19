8ae6b392c16dadc535f87160edb87ab78b2e2bb6
alisyabob <sjs939883@gmail.com>
2021-07-19 18:48:43 +0900

    매번 생성하던 리포지토리를 가장위에 선언해 재사용

--- springcore/src/main/java/com/sparta/springcore/ProductService.java
+++ springcore/src/main/java/com/sparta/springcore/ProductService.java
@@ -4,14 +4,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class ProductService {
+    // 멤버 변수 선언
+    private final ProductRepository productRepository;
+
+    // 생성자: ProductService() 가 생성될 때 호출됨
+    public ProductService() {
+        // 멤버 변수 생성
+        this.productRepository = new ProductRepository();
+    }
 
     public List<Product> getProducts() throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
+        // 멤버 변수 사용
         return productRepository.getProducts();
     }
 
     public Product createProduct(ProductRequestDto requestDto) throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
         // 요청받은 DTO 로 DB에 저장할 객체 만들기
         Product product = new Product(requestDto);
         productRepository.createProduct(product);
@@ -19,11 +26,6 @@ public class ProductService {
     }
 
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
-        if(requestDto.getMyprice()<0){
-            throw new IllegalAccessError("희망 최저가는 0 이하가 될 수 없습니다.");
-        }
-
-        ProductRepository productRepository = new ProductRepository();
         Product product = productRepository.getProduct(id);
         if (product == null) {
             throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
