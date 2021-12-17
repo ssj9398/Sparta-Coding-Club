ad1821031ba8ec80ad9a0bc391f4e90921d6bb6a
alisyabob <sjs939883@gmail.com>
2021-12-17 10:51:32 +0900

    render_template

--- 웹개발 종합반/projects/prac/app.py
+++ 웹개발 종합반/projects/prac/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('index.html')
 
 @app.route('/mypage')
 def mypage():
