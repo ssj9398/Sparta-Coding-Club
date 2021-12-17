4f0cfb50544c1576436da105e5427b693d685a8c
alisyabob <sjs939883@gmail.com>
2021-12-17 11:55:58 +0900

    Flask Get, Post

--- 웹개발 종합반/projects/prac/app.py
+++ 웹개발 종합반/projects/prac/app.py
@@ -1,17 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/test', methods=['GET'])
+def test_get():
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+
+@app.route('/test', methods=['POST'])
+def test_post():
+    title_receive = request.form['title_give']
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
 
 @app.route('/mypage')
 def mypage():
-   return 'mypage'
+    return 'mypage'
+
 
 @app.route('/button')
 def button():
-   return '<button>버튼</button>'
+    return '<button>버튼</button>'
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
